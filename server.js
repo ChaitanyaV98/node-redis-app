90c9a48333d484caa7f2ec9fc0a9465f6aa447ee
ChaitanyaV98 <chaitanya@betalectic.com>
2025-07-03 18:13:53 +0530

    created a basic redis client and connected to redis server and tested the redis connection

---
+++ server.js
@@ -0,0 +1,26 @@
+import redis from "redis";
+
+// create a client :  this client is used to interact with the redis server
+
+const client = redis.createClient({
+  host: "localhost", // this means the redis server is now hosted on local host
+  port: 6379, // default port that the redis server will use
+});
+
+// we will add event listener which will log to console if there is any error
+client.on("error", (error) => console.log("Redis client error occured", error));
+
+//simple function to test our redis connection
+
+async function testRedisConnection() {
+  try {
+    await client.connect();
+    console.log("client is now connected to redis server");
+  } catch (error) {
+    console.log("Error---", error);
+  } finally {
+    await client.quit(); // this helps in avoiding the open connections
+  }
+}
+
+testRedisConnection();
